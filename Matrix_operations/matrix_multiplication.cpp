d97521715672066422835c7edc441543f11d484b
SuzukiCCY <578229323@qq.com>
2023-10-08 17:41:31 +0800

    overload operator

--- Matrix_operations/matrix_multiplication.cpp
+++ Matrix_operations/matrix_multiplication.cpp
@@ -12,14 +12,11 @@ vector<vector<int>> B = {{1, 2, 3},
                         {4, 5, 6},
                         {7, 8, 9}}; 
 
-vector<vector<int>> C;
-vector<vector<int>> D;
 
-int main(){
-    C.reserve(3 * 3 * sizeof(int));
-    C.resize(3, vector<int>(3));
-    D.reserve(3 * 3 * sizeof(int));
-    D.resize(3, vector<int>(3));
+vector<vector<int>> MatrixHadaM(const vector<vector<int>> &A, const vector<vector<int>> &B){
+    int m = A.size();
+    int n = B[0].size();
+    vector<vector<int>> C(m, vector<int>(n));
 
     // Hadamard product, 逐元素乘积
     for(int i = 0; i < 3; ++i){
@@ -29,8 +26,14 @@ int main(){
         }
         cout << endl;
     }
-    
-    cout << endl;
+    return C;
+}
+
+
+vector<vector<int>> MatrixDotM(const vector<vector<int>> &A, const vector<vector<int>> &B){
+    int m = A.size();
+    int n = A[0].size();
+    vector<vector<int>> D(m, vector<int>(n));
 
     // Dot product, 矩阵点乘，标准矩阵相乘
     for(int i = 0; i < 3; ++i){
@@ -43,5 +46,24 @@ int main(){
         }
         cout << endl;
     }
+    return D;
+}
+
+
+vector<vector<int>> operator*(const vector<vector<int>> &A, const vector<vector<int>> &B){
+    return MatrixHadaM(A, B);
+}
+
+
+
+int main(){
+    vector<vector<int>> C;
+    C = MatrixHadaM(A, B);
+    cout << endl;
+    C = MatrixDotM(A, B);
+    cout << endl;
+
+    C = A * B;
+
     return 1;
 }
